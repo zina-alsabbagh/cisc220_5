8b49ca4c9c157000329e42b950f00f1b1178d5a5
Zina Al-Sabbagh <14zas@queensu.ca>
2016-12-01 23:07:25 -0500

    Final udll.h

---
+++ udll.h
@@ -0,0 +1,30 @@
+#ifndef UDLL_H
+#define UDLL_H
+
+typedef union Data {
+        char c;
+        char* charPtr;
+	int i;
+        int* intPtr;
+        float f;
+        float* floatPr; 	
+} Data;
+
+
+typedef struct node {}
+	int length;
+        union Data val;
+        struct node* next;
+        struct node* previous;   
+} Node;
+
+Node *head = NULL;
+Node *tail = NULL;
+
+void insert(int index, union Data data);
+
+void remove(int index);
+
+union Data get(int index);
+
+int length();
